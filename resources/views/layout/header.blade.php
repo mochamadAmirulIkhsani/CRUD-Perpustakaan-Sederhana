a2051b28e013bfd31ab96e10f7f12fe664adb63c
Mochamad Amirul Ikhsani <amirulikhsani41@gmail.com>
2025-06-06 01:13:27 +0700

    Auth Login & Logout

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +8,23 @@
     <title>Aplikasi Manajmenen Data Buku</title>
     <link rel="stylesheet" href="{{ asset('style/style.css') }}">
 </head>
+
 <body>
-<div class="container">
-    <h1>Manajemen Data Buku</h1>
-    <div class="nav">
-        <ul>
-            <li><a href="/kategori">Kategori</a></li>
-            <li><a href="/penerbit">Penerbit</a></li>
-            <li><a href="/buku">Buku</a></li>
-        </ul>
-    </div>
+    <div class="container">
+        <h1>Manajemen Data Buku</h1>
+        @if (Auth::check())
+            <div style="text-align: center; margin-bottom: 10px;">
+                <p style="margin-botton: 10px;">Anda Login sebagai <strong>{{ Auth::user()->name }}</strong></p>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="tombol">Logout</button>
+                </form>
+            </div>
+        @endif
+        <div class="nav">
+            <ul>
+                <li><a href="/kategori">Kategori</a></li>
+                <li><a href="/penerbit">Penerbit</a></li>
+                <li><a href="/buku">Buku</a></li>
+            </ul>
+        </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthManualController;
 use App\Http\Controllers\BukuController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\PenerbitController;
@@ -9,6 +10,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('kategori', KategoriController::class);
-Route::resource('penerbit', PenerbitController::class);
-Route::resource('buku', BukuController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('kategori', KategoriController::class)->middleware('auth');
+    Route::resource('penerbit', PenerbitController::class);
+    Route::resource('buku', BukuController::class);
+});
+
+
+
+//Route untuk login dan logout
+Route::get('/login', [AuthManualController::class, 'index'])->name('login');
+Route::post('/login', [AuthManualController::class, 'loginProses'])->name('loginProses');
+Route::post('/logout', [AuthManualController::class, 'logout'])->name('logout');
